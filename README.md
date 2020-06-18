fa486ea5799d72b11795b64fb4a82a58c22984c0
mgiang2015 <mgiang2015@gmail.com>
2020-06-18 21:20:28 +0800

    Added README.md. Added skeleton to build the cpu

---
+++ README.md
@@ -0,0 +1,17 @@
+To emulate:
+
+(memory map)
+CPU
+RAM
+PPU
+VRAM
+
+Screen / Graphic Driver
+
+Audio driver
+
+Cartridge
+
+Keypad
+
+ 
---
+++ src/dmg_cpu.rs
@@ -0,0 +1,37 @@
+// Declare all register ID and necessary constants
+
+pub struct Registers {
+    // 8 8-bit registers (including register F)
+    // 5 16-bit register (all pairs, not including AF, including PC)
+}
+
+
+pub struct CPU {
+    // All the components of CPU
+}
+
+impl CPU {
+    /*
+    pub fn initialize() -> Self {
+        // Initializing a Gameboy CPU (initial state)
+    }
+
+    pub fn tick() {
+        // For when CPU runs
+    }
+
+    pub fn read_opcode() {
+        // Obtain opcode
+    }
+
+    pub fn run_opcode(opcode: u8) {
+        // Let it run boi
+    }
+    */
+
+    // Opcodes goes here!!
+
+
+
+
+}
