7d41ee6d9e01e5737740bda3c2cf647e40f001c5
mgiang2015 <mgiang2015@gmail.com>
2020-06-27 00:36:47 +0800

    Add opcodes except rotate shift (2.5) and Bit operations (2.6 in the Programming Manual for GB)

--- src/dmg_cpu.rs
+++ src/dmg_cpu.rs
@@ -8,7 +8,7 @@ const CF: u8 = 0x10; // 0b00010000
 const A_ID: u8 = 0b111;
 const B_ID: u8 = 0b000;
 const C_ID: u8 = 0b001;
-const D_ID: u8 = 0b101;
+const D_ID: u8 = 0b010;
 const E_ID: u8 = 0b011;
 const H_ID: u8 = 0b100;
 const L_ID: u8 = 0b101;
@@ -82,13 +82,109 @@ impl CPU {
     pub fn handle_interrupt(&mut self) {
         // Implement how to handle interrupts, depending on registers IME, IF, IE
     }
+*/
 
-    pub fn execute_opcode() {
-        // Obtain opcode and do that bigass match fest
+    pub fn execute_opcode(&mut self) {
+        let opcode: u8 = self.mem[self.reg.PC as usize];
+        
+        let parts = (
+            opcode >> 6, // bit 7 6
+            (opcode & 0b0011_1000) >> 3, // bit 543
+            (opcode & 0b0000_0111) // bit 210
+        );
+
+        let next_pc = match parts {
+            // opcodes starting with 00
+            (0b00, 0b110, 0b110) => self.ld_addr_HL_n(),
+            (0b00, 0b001, 0b010) => self.ld_A_addr_BC(),
+            (0b00, 0b011. 0b010) => self.ld_A_addr_DE(),
+            (0b00, 0b000, 0b010) => self.ld_addr_BC_A(),
+            (0b00, 0b010, 0b010) => self.ld_addr_DE_A(),
+            (0b00, 0b111, 0b010) => self.ld_A_addr_HL_dec(),
+            (0b00, 0b110, 0b010) => self.ld_addr_HL_A_dec(),
+            (0b00, 0b101, 0b010) => self.ld_A_addr_HL_inc(),
+            (0b00, 0b100, 0b010) => self.ld_addr_HL_A_inc(),
+            (0b00, 0b001, 0b000) => self.ld_addr_nn_SP(),
+            (0b00, 0b011, 0b000) => self.jr_e(),
+            (0b00, 0b111, 0b111) => self.ccf(),
+            (0b00, 0b110, 0b111) => self.scf(),
+            (0b00, 0b000, 0b000) => self.nop(),
+            (0b00, 0b100, 0b111) => self.daa(),
+            (0b00, 0b101, 0b111) => self.cpl(),
+            (0b00, 0b110, 0b100) => self.inc_hl(),
+            (0b00, 0b110, 0b101) => self.dec_hl(),
+            
+            (0b00, 0b000..0b110, 0b001) => self.inc_ss(),
+            (0b00, 0b001..0b111, 0b011) => self.dec_ss(),
+            (0b00, 0b001..0b111, 0b001) => self.add_hlss(),
+            (0b00, 0b000..0b111, 0b101) => self.dec_r(),
+            (0b00, 0b000..0b111, 0b100) => self.inc_r(),
+            (0b00, 0b000..0b110, 0b001) => self.ld_rr_nn(),
+            (0b00, 0b100..0b111, 0b000) => self.jr_cc_e(),
+            (0b00, 0b000..0b111, 0b110) => self.ld_r_n(),
+
+            // opcodes starting with 01
+            (0b01, 0b110, 0b000..0b111) => self.ld_addr_HL_r(),
+            (0b01, 0b000..0b111, 0b110) => self.ld_r_addr_HL(),
+            (0b01, 0b000..0b111, 0b000..0b111) => self.ld_rx_ry(),
+
+            // opcodes starting with 10:
+            (0b10, 0b000, 0b110) => self.add_ahl(),
+            (0b10, 0b000, 0b000..0b111) => self.add_ar(),
+            (0b10, 0b001, 0b000..0b111) => self.adc_ar(),
+            (0b10, 0b001, 0b110) => self.adc_ahl(),
+            (0b10, 0b010, 0b000..0b111) => self.sub_r(),
+            (0b10, 0b010, 0b110) => self.sub_hl(),
+            (0b10, 0b011, 0b000..0b111) => self.sbc_ar(),
+            (0b10, 0b011, 0b110) => self.sbc_ahl(),
+            (0b10, 0b100, 0b000..0b111) => self.and_r(),
+            (0b10, 0b100, 0b110) => self.and_hl(),
+            (0b10, 0b110, 0b000..0b111) => self.or_r(),
+            (0b10, 0b110, 0b110) => self.or_hl(),
+            (0b10, 0b101, 0b000..0b111) => self.xor_r(),
+            (0b10, 0b101, 0b110) => self.xor_hl(),
+            (0b10, 0b111, 0b000..0b111) => self.cp_r(),
+            (0b10, 0b111, 0b110) => self.cp_hl(),
+
+            // opcodes starting with 11
+            (0b11, 0b111, 0b010) => self.ld_A_addr_nn(),
+            (0b11, 0b101, 0b010) => self.ld_addr_nn_A(),
+            (0b11, 0b110, 0b010) => self.ldh_A_addr_offset_C(),
+            (0b11, 0b100, 0b010) => self.ldh_addr_offset_C_A(),
+            (0b11, 0b110, 0b000) => self.ldh_A_addr_offset_n(),
+            (0b11, 0b100, 0b000) => self.ldh_addr_offset_n_A(),
+            (0b11, 0b111, 0b001) => self.ld_SP_HL(),
+            
+            (0b11, 0b000, 0b110) => self.add_an(),
+            (0b11, 0b001, 0b110) => self.adc_an(),
+            (0b11, 0b010, 0b110) => self.sub_n(),
+            (0b11, 0b011, 0b110) => self.sbc_an(),
+            (0b11, 0b100, 0b110) => self.and_n(),
+            (0b11, 0b110. 0b110) => self.or_n(),
+            (0b11, 0b101, 0b110) => self.xor_n(),
+            (0b11, 0b111, 0b110) => self.cp_n(),
+            (0b11, 0b101, 0b000) => self.add_spe(),
+
+
+
+            (0b11, 0b000, 0b011) => self.jp_nn(),
+            (0b11, 0b101, 0b001) => self.jp_HL(),
+            (0b11, 0b001, 0b101) => self.call_nn(),
+            (0b11, 0b001, 0b001) => self.ret(),
+            (0b11, 0b011, 0b001) => self.reti(),
+            (0b11, 0b110, 0b011) => self.di(),
+            (0b11, 0b111, 0b011) => self.ei(),
+            (0b11, 0b000..0b110, 0b101) => self.push_rr(),
+            (0b11, 0b000..0b110, 0b001) => self.pop_rr(),
+            (0b11, 0b000..0b011, 0b010) => self.jp_cc_nn(),
+            (0b11, 0b000..0b011, 0b100) => self.call_cc_nn(),
+            (0b11, 0b000..0b111, 0b111) => self.rst_n(),
+            (0b11, 0b000..0b011, 0b000) => self.ret_cc(),
+        }
+            
+
     }
 
-
-    */
 
     // Some reusable code (for opcodes)
     
@@ -492,25 +588,25 @@ impl CPU {
         ProgramCounter::Next(1)
     }
 
-    /// ld_A_addr_offset_C: Load contents of memory specified by C + 0xFF00 into A.
+    /// ldh_A_addr_offset_C: Load contents of memory specified by C + 0xFF00 into A.
     /// 1-byte instruction
-    pub fn ld_A_addr_offset_C(&mut self) -> ProgramCounter {
+    pub fn ldh_A_addr_offset_C(&mut self) -> ProgramCounter {
         self.load_mem_to_r8(A_ID, (0xFF00 + self.reg.C as u16));
 
         ProgramCounter::Next(1)
     }
 
-    /// ld_addr_offset_C_A: Load contents of A into memory specified by 0xFF00 + C.
+    /// ldh_addr_offset_C_A: Load contents of A into memory specified by 0xFF00 + C.
     /// 1-byte instruction
-    pub fn ld_addr_offset_C_A(&mut self) -> ProgramCounter {
+    pub fn ldh_addr_offset_C_A(&mut self) -> ProgramCounter {
         self.save_r8_to_mem(A_ID, (0xFF00 + self.reg.C as u16));
 
         ProgramCounter::Next(1)
     }
 
-    /// ld_A_addr_offset_nn: Load contents of memory specified by nn + 0xFF00 into A.
+    /// ldh_A_addr_offset_nn: Load contents of memory specified by nn + 0xFF00 into A.
     /// 1-byte instruction
-    pub fn ld_A_addr_offset_n(&mut self) -> ProgramCounter {
+    pub fn ldh_A_addr_offset_n(&mut self) -> ProgramCounter {
         let n = self.get_n();
 
         self.load_mem_to_r8(A_ID, (0xFF00 + n as u16));
@@ -518,9 +614,9 @@ impl CPU {
         ProgramCounter::Next(2)
     }
     
-    /// ld_addr_offset_n_A: Load contents of A into memory specified by 0xFF00 + n.
+    /// ldh_addr_offset_n_A: Load contents of A into memory specified by 0xFF00 + n.
     /// 1-byte instruction
-    pub fn ld_addr_offset_n_A(&mut self) -> ProgramCounter {
+    pub fn ldh_addr_offset_n_A(&mut self) -> ProgramCounter {
         let n = self.get_n();
 
         self.save_r8_to_mem(A_ID, (0xFF00 + n as u16));
