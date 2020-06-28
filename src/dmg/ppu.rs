7fbfa26c2095fcda6a76cc53489ee8af320fa6d3
mgiang2015 <mgiang2015@gmail.com>
2020-06-28 15:47:02 +0800

    Add PPU constants

--- src/dmg/ppu.rs
+++ src/dmg/ppu.rs
@@ -30,3 +30,25 @@ const BLACK: Color = Color {
     b: 32,
     a: 255,
 };
+
+pub const OAM_SIZE: usize = 0x100; // address for OAM
+const FRAMEBUFFER_SIZE: usize = DISPLAY_WIDTH * DISPLAY_HEIGHT; // address for the full frame
+
+const CLKS_SCREEN_REFRESH: u32 = 70224; // refresh every 70224 clks
+pub const DISPLAY_WIDTH: usize = 160;
+pub const DISPLAY_HEIGHT: usize = 144;
+
+pub const VRAM_SIZE: usize = 1024*16; // 16KB Vram
+
+const MODE_HBLANK: u32 = 0;
+const MODE_VBLANK: u32 = 1;
+const MODE_OAM: u32 = 2;
+const MODE_VRAM: u32 = 3;
+
+const HBLANK_CYCLES: u32 = 204;
+const VBLANK_CYCLES: u32 = 456;
+const OAM_CYCLES: u32 = 80;
+const VRAM_CYCLES: u32 = 172;
+
+
+
