b4b45c4dc93c6cdbc428976e8ed30746f31b5ec2
Theodore L <theodoreleebrant@gmail.com>
2020-06-28 15:20:50 +0800

    add colors

---
+++ src/dmg/ppu.rs
@@ -0,0 +1,32 @@
+#[derive(Debug,PartialEq,Eq)]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+const WHITE: Color = Color {
+    r: 224,
+    g: 248,
+    b: 208,
+    a: 255,
+};
+const LIGHT_GRAY: Color = Color {
+    r: 136,
+    g: 192,
+    b: 112,
+    a: 255,
+};
+const DARK_GRAY: Color = Color {
+    r: 39,
+    g: 80,
+    b: 70,
+    a: 255,
+};
+const BLACK: Color = Color {
+    r: 8,
+    g: 24,
+    b: 32,
+    a: 255,
+};
