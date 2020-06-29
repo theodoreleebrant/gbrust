e37fff903e67676964b36a09063440e35a5225b8
Theodore L <theodoreleebrant@gmail.com>
2020-06-30 00:43:16 +0800

    update mod file

--- src/dmg/mod.rs
+++ src/dmg/mod.rs
@@ -2,8 +2,12 @@ pub mod dmg_cpu;
 pub mod cart;
 pub mod ppu;
 pub mod interconnect;
+pub mod gamepad;
+pub mod console;
 
 pub use self::cart::*;
 pub use self::dmg_cpu::*;
 pub use self::ppu::*;
 pub use self::interconnect::*;
+pub use self::gamepad::*;
+pub use self::console::*;
