a0db61e983fb9a79357d3ba7436a2d4283b4e4f9
Theodore L <theodoreleebrant@gmail.com>
2020-06-29 22:26:54 +0800

    add read memory fn

--- src/dmg/interconnect.rs
+++ src/dmg/interconnect.rs
@@ -4,14 +4,14 @@ use super::timer::Timer;
 use super::gamepad::Gamepad;
 
 const RAM_SIZE: usize = 32 * 1024; // Memory for the last 32KB as first 32KB is for ROM
-const HW_IO_REG: usize = 0x7f; //Hardware IO register memory FF00-FF7F; impl as hwram
+const ZERO_PAGE: usize = 0x7f;
 
 pub struct Interconnect {
     pub cart: Cart,
     ppu: Ppu,
     ram: Box<[u8]>,      
-    hwram: Box<[u8]>,
-    ppu_dma: u8,
+    zero_page: Box<[u8]>,
+    ppu_dma: u8, // DMA Transfer and Start Address, 0xFF46
     pub int_enable: u8,
     pub int_flags: u8,
     ram_offset: usize,
@@ -30,7 +30,7 @@ impl Interconnect {
             // timer: Timer::new(),
             // gamepad: gamepad,
             ram: vec![0; RAM_SIZE].into_boxed_slice(),
-            hwram: vec![0; HW_IO_REG].into_boxed_slice(),
+            zero_page: vec![0; HW_IO_REG].into_boxed_slice(),
             ppu_dma: 0,
             int_enable: 0,
             int_flags: 0,
@@ -38,6 +38,67 @@ impl Interconnect {
         }
     }
 
+    pub fn read(&mut self, addr: u16) -> u8 {
+        match addr {
+            // For more information: http://gameboy.mongenel.com/dmg/asmmemmap.html
+            0x0000...0x7fff => self.cart.read(addr), // Cartridge ROM
+            0x8000...0x9fff => self.ppu.read(addr), // Picture Processing Unit
+            0xa000...0xbfff => 0, // Cartridge swappable RAM
+            0xc000...0xdfff => self.ram[(addr - 0xc000) as usize], // Internal RAM
+            // Might cause problems in GBC implementation but for DMG should be ok
+            0xe000...0xfdff => self.read(addr - 0xe000 + 0xc000), 
+            // Echo memory. Just copies over 0xc000..oxcfff
+
+            // PPU addresses
+            0xfe00...0xfe9f // Object Attribute Memory, in PPU / Sprite RAM
+                | 0xff40...0xff45 // LCDC, LCDStat, SCY, SCX, LY, LYC
+                | 0xff47...0xff4b // BGP, Object Palette Data 0-1, WY, WX, 
+                | 0xff68...0xff69 // LCD Color Palette for CGB
+                | 0xff4f => { // Destination Memory Bank
+                self.ppu.read(addr)
+            }
+
+            // Unused memory
+            0xfea0...0xfeff => 0,
+
+            // 0xFF00 - 0xFF7F: Hardware I/O Registers
+            // Details http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf pg35
+            // 0xFF00: Gamepad
+            0xff00 => self.gamepad.read(),
+
+            // 0xFF01 - 0xFF02: serial I/O, used for linking up to other gameboy
+            0xff01...0xff02 => 0,
+            
+            // 0xFF04: DIV/Divider Register, incremented 16384 times a second.
+            //         Needs to be implemented in timer.
+            // 0xFF05: TIMA / Timer Counter - Interrupt when overflow.
+            // 0xFF06: TMA / Timer Modulo - used when TIMA overflows.
+            // 0xFF07: TAC / Timer Control. 3 bits: MSB is stop/start timer (0/1)
+            //         2LSB is input clock speed (4096kHz, 262114 kHz, 65536kHz, 16384kHz)
+            0xff04...0xff07 => 0,
+
+            // 0xFF08 - 0xFFOE unused
+
+            // 0xFFOF - IF / Interrupt Flag
+            0xff0f => self.int_flags,
+
+            // 0xFFFF - IE / Interupt Enable
+            0xffff => self.int_enable,
+
+            // 0xFF10 - 0xFF3F: SPU (Not implemented yet)
+            0xff10...0xff3f => 0,
+
+            // http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf pg 55
+            0xff46 => self.ppu_dma,
+
+            // Unusable memory, used as a speed switch (TODO)
+            // 0xff4d => 0, 
+            0xff80...0xfffe => self.zero_page[(addr - 0xff80) as usize],
+            
+            _ => panic!("Read: addr not in range: 0x{:x}", addr),
+        }
+    }
+}
     pub fn write(&mut self, addr: u16, val: u8) {
         match addr {
             // Cartridge rom
