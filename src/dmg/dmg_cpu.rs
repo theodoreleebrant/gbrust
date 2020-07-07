525e484d33198b1285cb377cbba58e4f05709c54
mgiang2015 <mgiang2015@gmail.com>
2020-07-08 01:25:51 +0800

    Fix overflow error for functions that increment hl

--- src/dmg/dmg_cpu.rs
+++ src/dmg/dmg_cpu.rs
@@ -863,7 +863,7 @@ impl Cpu {
     /// 1-byte instruction.
     pub fn ld_addr_hl_a_inc(&mut self) -> ProgramCounter {
         self.save_r8_to_mem(A_ID, self.reg.hl);
-        self.reg.hl += 1;
+        self.reg.hl.wrapping_add(1);
 
         ProgramCounter::Next(1, 2)
     }
@@ -873,7 +873,7 @@ impl Cpu {
     /// 1-byte instruction.
     pub fn ld_addr_hl_a_dec(&mut self) -> ProgramCounter {
         self.save_r8_to_mem(A_ID, self.reg.hl);
-        self.reg.hl -= 1;
+        self.reg.hl.wrapping_sub(1);
 
         ProgramCounter::Next(1, 2)
     }
