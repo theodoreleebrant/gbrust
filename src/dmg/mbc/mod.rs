ad7eecb73d5a4ad52852f65c6af997165298634f
mgiang2015 <mgiang2015@gmail.com>
2020-07-24 22:26:11 +0800

    Modify mod.rs for mbc to include mbc1 and mbc2

--- src/dmg/mbc/mod.rs
+++ src/dmg/mbc/mod.rs
@@ -1,14 +1,14 @@
 // This file defines default trait for all MBCs and RamInfo, RomInfo
 mod mbc_properties;
 mod rom_only;
-//mod mbc1;
-//mod mbc2;
+mod mbc1;
+mod mbc2;
 //mod mbc3;
 //mod mbc5;
 
 pub use self::mbc_properties::*;
 pub use self::rom_only::*;
-// pub use self::mbc1::*;
-// pub use self::mbc2::*;
+pub use self::mbc1::*;
+pub use self::mbc2::*;
 // pub use self::mbc3::*;
 // pub use self::mbc5::*;
